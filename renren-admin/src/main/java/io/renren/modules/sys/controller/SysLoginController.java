427289f0ac9eb971315922b2cd23b1d72662eb1a
lidh <1391099066@qq.com>
2019-12-18 18:42:59 +0800

    修改主页样式

--- renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
@@ -59,11 +59,11 @@ public class SysLoginController {
 	@ResponseBody
 	@RequestMapping(value = "/sys/login", method = RequestMethod.POST)
 	public R login(String username, String password, String captcha) {
-		String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
-		if(!captcha.equalsIgnoreCase(kaptcha)){
-			return R.error("验证码不正确");
-		}
-		
+//		String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
+//		if(!captcha.equalsIgnoreCase(kaptcha)){
+//			return R.error("验证码不正确");
+//		}
+//
 		try{
 			Subject subject = ShiroUtils.getSubject();
 			UsernamePasswordToken token = new UsernamePasswordToken(username, password);
